61b58662c8ab3fb1cb131098555ae1cbb59da440
Kumar <24rkumar@student.dasd.org>
2021-01-11 09:52:37 -0500

    Initial commit

---
+++ multiplication/algorithm_test.py
@@ -0,0 +1,20 @@
+import time
+
+from multiplication import *
+
+num1 = 34767645
+num2 = 98452365
+
+start_time = time.time()
+
+for i in range(0, 10000):
+    foo = RecIntMult.mult(num1, num2)
+
+print(f"RecIntMult: {time.time() - start_time}")
+
+start_time = time.time()
+
+for i in range(0, 10000):
+    foo = Karatsuba.karatsuba_mult(num1, num2)
+
+print(f"Karatsuba: {time.time() - start_time}")
